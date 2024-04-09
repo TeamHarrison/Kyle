1eab8be512993bd8864d696e66d167da6cdf26c9
Kyle Harrison <RedactedProfile@users.noreply.github.com>
2024-04-09 11:31:39 -0700

    00 blog post updated

--- posts/00-hi-world.php
+++ posts/00-hi-world.php
@@ -6,6 +6,9 @@ $date = "April 8th 2024";
 <?php require_once('../header.php'); ?>
 
 <h1><?=$page_title;?></h1>
-<p>Hi there</p>
+<p>Hi 👋</p>
+<p></p>
+<p>As far as personal blogs go, I'm still deciding what to post here.</p>
+<p>But I do have an 'interesting' system here that I quite like. Nice and simple, easy to maintain, and completely flexible. Maybe one day I'll go into details of what i've done here.</p>
 
 <?php require_once('../footer.php'); ?>
