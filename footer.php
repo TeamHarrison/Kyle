25777d7e3d30acd4fc8a787244ed6c58c5d73b2c
Kyle Harrison <RedactedProfile@users.noreply.github.com>
2024-04-09 11:32:23 -0700

    added posts link to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<p><code><a href="/">[Home]</a> <a href="/stuff.php">[Stuff]</a>&nbsp;<a href="/about.php">[AboutMe]</a></code></p>
+<p><code><a href="/">[Home]</a> <a href="/stuff.php">[Stuff]</a> <a href="/posts.php">[Posts]</a> <a href="/about.php">[AboutMe]</a></code></p>
 <p><code><a href="https://github.com/RedactedProfile" target="_blank">[GitHub]</a></code></p>
 <p><code>&copy; Kyle Harrison <?=date("Y");?></code></p>
 </body>
