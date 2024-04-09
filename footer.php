25fa4bdf6d3804060e3989f414cecb8df98bee4a
Kyle Harrison <RedactedProfile@users.noreply.github.com>
2024-04-09 12:45:06 -0700

    GA4

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <p><code><a href="/">[Home]</a> <a href="/stuff.php">[Stuff]</a> <a href="/posts.php">[Posts]</a> <a href="/about.php">[AboutMe]</a></code></p>
 <p><code><a href="https://github.com/RedactedProfile" target="_blank">[GitHub]</a></code></p>
 <p><code>&copy; Kyle Harrison <?=date("Y");?></code></p>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TE4Y82D1CZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TE4Y82D1CZ');
+</script>
 </body>
 </html>
