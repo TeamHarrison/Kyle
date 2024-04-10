31a7d6d56480567c39a7ff033dd635f84f3bedde
Kyle Harrison <RedactedProfile@users.noreply.github.com>
2024-04-09 17:13:50 -0700

    mary gear solid post

--- projects/game-mary-gear-solid.php
+++ projects/game-mary-gear-solid.php
@@ -3,3 +3,23 @@ $page_title = "Mary Gear Solid";
 $project_category = "Game";
 $date = "April 9th 2024";
 ?>
+<?php require_once('../header.php'); ?>
+<h1><?=$page_title;?></h1>
+
+<h2>Backstory.</h2>
+<p>In January of 2019, just before the first Canadian COVID lockdown, I told myself I was going to stop using Unity for goofs and start making playable things with it. I did some little experiements with it, nothing too crazy. I remember a test dialogue system for some kind of action RPG type experience, but it was a <em>little</em> ambitious for what I was capable of at the time.</p>
+<p>So I thought to myself "Game jams.", a perfect way to hit the ground running and learn along the way while keeping scope and ambition in check.</p>
+<p>I signed up for the <a href="https://itch.io/jam/7d3d-3" target="_blank">3rd 7D3D (7 Day 3D GameJam)</a>. There was no theme, there were very liberal restrictions, basically all I had to worry about was delivering something within the 7 day time allotment.</p>
+<p>I remember struggling to think of something approachable in that time limit. I had never really built anything tangibly worth playing in Unity before either. I needed to keep whatever I was going to do, realistic and focused.</p>
+<p>During an evening where I was driving my family home, I was listening to some game soundtracks, and in particular one stood out to me as the muse smacked my head and told me to pay attention: Metal Gear Solid, and specifically: the sneaking mechanic. Yes... yes indeed... how hard would that sneaking mechanic be to implement in a quick experimental way.  How would the guards work? What would the map look like? Why are you sneaking around?</p> 
+<p>I had a million little questions pop into my head, and one by one I was able to answer them in my head as well.  Everything was popping into clear focus.  This was it. This was the idea, and as luck would have it.. the jam was about to start.</p>
+<p>For the theme of the project, not that it needed a story of any kind, I decided to take a look inward. As a software developer who's worked in the past for big companies like EA, and small companies like boutique web shoppes, and my love of movies like Office Space, the thought of having to stay late because you're beholden to your boss is one hell of a killer thought.  What if, what if you were trying to sneak out of the office like Peter failed to do in Office Space?</p>
+<p>Yes. Yes of course. And that answered the next question of whether there needed to be any combat involved in the gameplay. No. It can be stricly sneaking and nothing else.</p>
+<p>But what about the antagonists? How we do justify their narrow field of view?  Of course, this is a tech company... their eyes are glued to their phone's. That could be why they're mindlessly patrolling the hallway even, "looking busy".</p>
+<p>Everything was taking shape. I just needed a title. I wanted to keep the "MGS" for SEO purposes, but maybe the M stands for a name?  The moment I said "Mary" in my head, I knew that had to be it.</p>
+<p>It's not like it's a glamorous or heroic role to play, but I've always had a preference for female protagonists and heroes, and in a way I could help do my part to bring at least a modicum of representation.</p>
+<p>So there it was. I had an answer for every question, and I hadn't even stepped out of my vehicle yet. I couldn't wait to get to work.</p>
+
+<p>That week is a blur to me.  Not because anything went wrong, but because everything kept going to right and smooth not much stood out as far as tales to regale. I used MakeHuman to create Mary and the Worker, I did the rigging and simple animation, and I sourced office related models from from sources like Turbosquid. I learned on the spot how to make the animation controller work, got input in very quickly, even got to learn about Navmesh's and patrol routes for the NPC's. I used an asset (at the time) called Cinemachine that allowed me to script the Camera super easily from follow mode to corner-cam mode</p>
+
+<?php require_once('../footer.php'); ?>
