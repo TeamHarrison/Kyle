5d682bd522e2caa4731851de789e13a0307db708
Kyle Harrison <redactedprofile@gmail.com>
2024-04-10 20:34:21 -0700

    enabled project roll

--- stuff.php
+++ stuff.php
@@ -10,6 +10,11 @@ $projects = Project::Collect();
 <h1>Stuff.</h1>
 <p>Over the years I've built a number of things, contributed to a number of projects. Here I hope to chronicle at least the most interesting.</p>
 <p></p>
+<ul>
+<?php foreach($projects as $project): ?>
+    <li><a href="/projects/<?=$project->filename;?>"><?=$project->title;?></a> (<?=$project->date->format("F jS, Y");?>)</li>
+<?php endforeach; ?>
+</ul>
 <p>Coming Soon.</p>
 <p></p>
 <?php require_once('footer.php'); ?>
