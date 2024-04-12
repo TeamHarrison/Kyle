2cc787f6d8a25211cd5afb0bb1854967cb81dc45
Kyle Harrison <redactedprofile@gmail.com>
2024-04-12 03:26:40 -0700

    update flappy

--- projects/game-flappybird.php
+++ projects/game-flappybird.php
@@ -13,6 +13,10 @@ $date = "April 12th 2024";
 <h2>Backstory.</h2>
 <p>Long story short, I wanted to try out libGDX for my own amusement. I was watching a CodeBullet video where Mr. Bullet was making a bunch of really weird flappy bird clones using unorthodox display methods (such as using Windows Explorer as the renderer). I wondered how long it might take me to make a half way decent Flappy Bird clone myself, and I'd been dying to trying LibGDX for <em>something</em></p>
 
-<h3>Check out the <a href="https://ninja-ghost.itch.io/flappy" target="_blank">page for download</a></h3>
+<br />
+<br />
+<h3>Check out the <a href="https://ninja-ghost.itch.io/flappy" target="_blank">itch.io page for download</a></h3>
+<br />
+<br />
 
 <?php require_once('../footer.php'); ?>
